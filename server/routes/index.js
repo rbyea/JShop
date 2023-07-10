0a4ca7018486d62e317a226aca3a9b4067e649c6
Egor <csgo.roby@gmail.com>
2023-07-11 00:07:04 +0300

    10.07 server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
+router.use("/auth", require("./auth.routes"));
 router.use("/features", require("./features.routes"));
 router.use("/categories", require("./categories.routes"));
+router.use("/user", require("./user.routes"));
 
 module.exports = router;
