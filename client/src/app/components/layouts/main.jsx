a1512f363d0486e2d6f4b41cb5835bca5157d9cd
Egor <csgo.roby@gmail.com>
2023-07-30 13:33:43 +0300

    ud

--- client/src/app/components/layouts/main.jsx
+++ client/src/app/components/layouts/main.jsx
@@ -10,8 +10,9 @@ import { getLoadingStatusCategories } from "../../store/categoriesSlice";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import SliderCard from "../common/sliders/sliderItem";
 import Button from "../ui/button";
-import GameListCard from "../common/gameList/gameListCard";
 import { getTopSalesGames } from "../../store/gamesSlice";
+import { discountFunc } from "../../utils/discountFunc";
+import { Link } from "react-router-dom";
 
 const Main = (props) => {
   React.useEffect(() => {
@@ -99,22 +100,62 @@ const Main = (props) => {
         )}
       </>
       <section className="pb-5">
-      <div className="container">
-        <div className="d-flex align-item-center title mb-3">
-          <h5 className="m-0 font-weight-normal">Лидеры продаж</h5>
-          <Button
-            link="catalog/"
-            title="Смотреть все"
-            name="btn-sm btn-outline-light ml-auto"
-          />
+        <div className="container">
+          <div className="d-flex align-item-center title mb-3">
+            <h5 className="m-0 font-weight-normal">Лидеры продаж</h5>
+            <Button
+              link="catalog/"
+              title="Смотреть все"
+              name="btn-sm btn-outline-light ml-auto"
+            />
+          </div>
+          <div className="row">
+            {data.map((game) => (
+              <div key={game._id} className="col-lg-3 col-md-6">
+                <div className="osahan-card">
+                  <Link to={`/card/${game._id}`}>
+                    <div className="osahan-card__image">
+                      <img className="img-fluid" src={game.picture} alt="" />
+                    </div>
+                    <div className="osahan-card-body mt-3">
+                      <h6 className="text-white mb-1">{game.title}</h6>
+                      {data && <p className="mb-0 text-white-50">{data}</p>}
+                      {game.price && game.discount && (
+                        <div className="price mt-3">
+                          {Number(game.discount) !== 0 && (
+                            <>
+                              <div
+                                className={`${
+                                  Number(game.discount) > 20
+                                    ? "bg-danger"
+                                    : "bg-primary"
+                                }  d-inline-block text-center px-2 py-1 text-white`}
+                              >
+                                -{game.discount}%
+                              </div>
+                              <del className="text-white-50 mx-1">
+                                {game.price} руб.
+                              </del>
+                            </>
+                          )}
+
+                          <span className="text-white">
+                            {game.price -
+                              discountFunc(game.price, game.discount)}
+                            руб.
+                          </span>
+                        </div>
+                      )}
+                    </div>
+                  </Link>
+                </div>
+              </div>
+
+              // <GameListCard key={game._id} {...game} />
+            ))}
+          </div>
         </div>
-        <div className="row">
-          {data.map((game) => (
-            <GameListCard key={game._id} {...game} />
-          ))}
-        </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
