db654e8bfedbe8040150835c39080d162f4af696
Egor <csgo.roby@gmail.com>
2023-07-30 09:50:50 +0300

    update3

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import Main from "./components/layouts/main";
 import Header from "./components/common/Header/Header";
 import LoaderDate from "./components/ui/hoc/loaderDate";
@@ -13,10 +13,20 @@ import Login from "./components/layouts/login";
 import ProtectedRoute from "./components/ui/protectedRoute";
 import PersonalAccount from "./components/layouts/personalAccount";
 import NotFound from "./components/layouts/NotFound";
+import Navbar from "./components/ui/navbar";
+import LogoJSHOP from "./assets/logo.svg";
 
 function App() {
   return (
     <LoaderDate>
+      <nav className="navbar navbar-expand navbar-dark bg-dark osahan-nav-top p-0 px-2 sticky-top">
+        <div className="container">
+          <Link className="navbar-brand mr-2" to="/">
+            <img src={LogoJSHOP} alt="" />
+          </Link>
+          <Navbar />
+        </div>
+      </nav>
       <Header />
       <Switch>
         <Route path="/catalog" component={Catalog} />
